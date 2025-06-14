90d9fd5e9cc31299e8506b7fe0124a1a760b9df8
dvbdbb <dovy1@hotmail.co.uk>
2025-06-14 10:41:59 +0100

    Add more products, refactor seeding file with reduced repetition

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,246 +1,166 @@
-import { v4 as uuid } from "uuid";
-
-import { PrismaClient } from "../generated/prisma";
-import { processDateForClient, slugify } from "../src/lib/utils";
+import { Prisma, PrismaClient } from "../generated/prisma";
+import { slugify } from "../src/lib/utils";
+import { Categories, Sizes } from "@/lib/definitions";
 
 const prisma = new PrismaClient();
 
+const generatePrice = () => (25 + Math.floor(Math.random() * (300 - 25))) * 100; // random between £25 and £299 in p
+
+const products = [
+    {
+        name: "Black & large white graphic",
+        gender: "womens",
+        price: generatePrice(),
+        src: "/tshirt1.jpg",
+        alt: "Short black t-shirt with large white graphic",
+        size: ["xs", "s", "m", "l", "xl"],
+    },
+    {
+        name: "White & black company logo",
+        gender: "womens",
+        price: generatePrice(),
+        src: "/tshirt2.jpg",
+        alt: "White t-shirt with black company name",
+        size: ["xs", "s", "m", "l", "xl"],
+    },
+    {
+        name: "Black & small white print",
+        gender: "womens",
+        price: generatePrice(),
+        src: "/tshirt7.jpg",
+        alt: "Black t-shirt with small white text",
+        size: ["xs", "s", "m", "l", "xl"],
+    },
+    {
+        name: "Black & small white print 2",
+        gender: "womens",
+        price: generatePrice(),
+        src: "/tshirt8.jpg",
+        alt: "Black t-shirt with small white text",
+        size: ["xs", "s", "m", "l", "xl"],
+    },
+    {
+        name: "White & multicolour paint graphic",
+        gender: "womens",
+        price: generatePrice(),
+        src: "/tshirt10.jpg",
+        alt: "White t-shirt with multicolour paint graphic",
+        size: ["xs", "s", "m", "l", "xl"],
+    },
+    {
+        name: "Black & orange logo",
+        gender: "womens",
+        price: generatePrice(),
+        src: "/tshirt11.jpg",
+        alt: "Black t-shirt with striking orange logo",
+        size: ["xs", "s", "m", "l", "xl"],
+    },
+    {
+        name: "Black & white striped",
+        gender: "womens",
+        price: generatePrice(),
+        src: "/tshirt14.jpg",
+        alt: "Black & white striped t-shirt with single-word text",
+        size: ["xs", "s", "m", "l", "xl"],
+    },
+    {
+        name: "White & black text graphic",
+        gender: "womens",
+        price: generatePrice(),
+        src: "/tshirt15.jpg",
+        alt: "White t-shirt with black text graphic",
+        size: ["xs", "s", "m", "l", "xl"],
+    },
+    {
+        name: "White & small black graphic",
+        gender: "mens",
+        price: generatePrice(),
+        src: "/tshirt3.jpg",
+        alt: "White t-shirt with small black graphic",
+        size: ["s", "m", "l", "xl", "xxl"],
+    },
+    {
+        name: "White & large black logo",
+        gender: "mens",
+        price: generatePrice(),
+        src: "/tshirt4.jpg",
+        alt: "White t-shirt with large black company logo",
+        size: ["s", "m", "l", "xl", "xxl"],
+    },
+    {
+        name: "Blue & large white graphic",
+        gender: "mens",
+        price: generatePrice(),
+        src: "/tshirt5.jpg",
+        alt: "Blue t-shirt with large white text graphic",
+        size: ["s", "m", "l", "xl", "xxl"],
+    },
+    {
+        name: "Black & medium mixed print",
+        gender: "mens",
+        price: generatePrice(),
+        src: "/tshirt6.jpg",
+        alt: "Black t-shirt with medium mixed-colour print",
+        size: ["s", "m", "l", "xl", "xxl"],
+    },
+    {
+        name: "Red & small black logo",
+        gender: "mens",
+        price: generatePrice(),
+        src: "/tshirt9.jpg",
+        alt: "Red t-shirt with small black logo",
+        size: ["s", "m", "l", "xl", "xxl"],
+    },
+    {
+        name: "Black & white text graphic",
+        gender: "mens",
+        price: generatePrice(),
+        src: "/tshirt12.jpg",
+        alt: "Black t-shirt with styled white text graphic",
+        size: ["s", "m", "l", "xl", "xxl"],
+    },
+    {
+        name: "Dark grey & small logo",
+        gender: "mens",
+        price: generatePrice(),
+        src: "/tshirt13.jpg",
+        alt: "Dark grey t-shirt with small yellow logo",
+        size: ["s", "m", "l", "xl", "xxl"],
+    },
+    {
+        name: "Black & small company logo",
+        gender: "mens",
+        price: generatePrice(),
+        src: "/tshirt16.jpg",
+        alt: "Black t-shirt with small multicolour company logo",
+        size: ["s", "m", "l", "xl", "xxl"],
+    },
+];
+
 async function main() {
-    const createProductOne = async () => {
-        const name = "T-shirt design 1";
-
+    for (let product of products) {
         const createdProduct = await prisma.product.create({
             data: {
-                name,
-                gender: "womens",
-                price: 2200,
-                slug: slugify(name),
-                src: "/tshirt1.jpg",
-                alt: "T-shirt design 1",
+                name: product.name,
+                gender: product.gender as Categories,
+                price: product.price,
+                slug: slugify(product.name),
+                src: product.src,
+                alt: product.alt,
                 dateAdded: new Date(),
             },
         });
 
-        await prisma.stock.createMany({
-            data: [
-                {
-                    size: "xs",
-                    quantity: Math.floor(Math.random() * 20),
-                    productId: createdProduct.id,
-                },
-                {
-                    size: "s",
-                    quantity: Math.floor(Math.random() * 20),
-                    productId: createdProduct.id,
-                },
-                {
-                    size: "m",
-                    quantity: Math.floor(Math.random() * 20),
-                    productId: createdProduct.id,
-                },
-                {
-                    size: "l",
-                    quantity: Math.floor(Math.random() * 20),
-                    productId: createdProduct.id,
-                },
-                {
-                    size: "xl",
-                    quantity: Math.floor(Math.random() * 20),
-                    productId: createdProduct.id,
-                },
-            ],
-        });
-    };
-
-    const createProductTwo = async () => {
-        const name = "T-shirt design 2";
-
-        const createdProduct = await prisma.product.create({
-            data: {
-                name,
-                gender: "womens",
-                price: 3600,
-                slug: slugify(name),
-                src: "/tshirt2.jpg",
-                alt: "T-shirt design 2",
-                dateAdded: new Date(),
-            },
-        });
+        const stockEntries: Prisma.StockCreateManyInput[] = product.size.map((size) => ({
+            size: size as Sizes,
+            quantity: Math.floor(Math.random() * 20),
+            productId: createdProduct.id,
+        }));
 
         await prisma.stock.createMany({
-            data: [
-                {
-                    size: "xs",
-                    quantity: Math.floor(Math.random() * 20),
-                    productId: createdProduct.id,
-                },
-                {
-                    size: "s",
-                    quantity: Math.floor(Math.random() * 20),
-                    productId: createdProduct.id,
-                },
-                {
-                    size: "m",
-                    quantity: Math.floor(Math.random() * 20),
-                    productId: createdProduct.id,
-                },
-                {
-                    size: "l",
-                    quantity: Math.floor(Math.random() * 20),
-                    productId: createdProduct.id,
-                },
-                {
-                    size: "xl",
-                    quantity: Math.floor(Math.random() * 20),
-                    productId: createdProduct.id,
-                },
-            ],
+            data: stockEntries,
         });
-    };
-
-    const createProductThree = async () => {
-        const name = "T-shirt design 3";
-
-        const createdProduct = await prisma.product.create({
-            data: {
-                name,
-                gender: "mens",
-                price: 8200,
-                slug: slugify(name),
-                src: "/tshirt3.jpg",
-                alt: "T-shirt design 3",
-                dateAdded: new Date(),
-            },
-        });
-
-        await prisma.stock.createMany({
-            data: [
-                {
-                    size: "s",
-                    quantity: Math.floor(Math.random() * 20),
-                    productId: createdProduct.id,
-                },
-                {
-                    size: "m",
-                    quantity: Math.floor(Math.random() * 20),
-                    productId: createdProduct.id,
-                },
-                {
-                    size: "l",
-                    quantity: Math.floor(Math.random() * 20),
-                    productId: createdProduct.id,
-                },
-                {
-                    size: "xl",
-                    quantity: Math.floor(Math.random() * 20),
-                    productId: createdProduct.id,
-                },
-                {
-                    size: "xxl",
-                    quantity: Math.floor(Math.random() * 20),
-                    productId: createdProduct.id,
-                },
-            ],
-        });
-    };
-    const createProductFour = async () => {
-        const name = "T-shirt design 4";
-
-        const createdProduct = await prisma.product.create({
-            data: {
-                name,
-                gender: "mens",
-                price: 11000,
-                slug: slugify(name),
-                src: "/tshirt4.jpg",
-                alt: "T-shirt design 4",
-                dateAdded: new Date(),
-            },
-        });
-
-        await prisma.stock.createMany({
-            data: [
-                {
-                    size: "s",
-                    quantity: Math.floor(Math.random() * 20),
-                    productId: createdProduct.id,
-                },
-                {
-                    size: "m",
-                    quantity: Math.floor(Math.random() * 20),
-                    productId: createdProduct.id,
-                },
-                {
-                    size: "l",
-                    quantity: Math.floor(Math.random() * 20),
-                    productId: createdProduct.id,
-                },
-                {
-                    size: "xl",
-                    quantity: Math.floor(Math.random() * 20),
-                    productId: createdProduct.id,
-                },
-                {
-                    size: "xxl",
-                    quantity: Math.floor(Math.random() * 20),
-                    productId: createdProduct.id,
-                },
-            ],
-        });
-    };
-    const createProductFive = async () => {
-        const name = "T-shirt design 5";
-
-        const createdProduct = await prisma.product.create({
-            data: {
-                name,
-                gender: "mens",
-                price: 20500,
-                slug: slugify(name),
-                src: "/tshirt5.jpg",
-                alt: "T-shirt design 5",
-                dateAdded: new Date(),
-            },
-        });
-
-        await prisma.stock.createMany({
-            data: [
-                {
-                    size: "s",
-                    quantity: Math.floor(Math.random() * 20),
-                    productId: createdProduct.id,
-                },
-                {
-                    size: "m",
-                    quantity: Math.floor(Math.random() * 20),
-                    productId: createdProduct.id,
-                },
-                {
-                    size: "l",
-                    quantity: Math.floor(Math.random() * 20),
-                    productId: createdProduct.id,
-                },
-                {
-                    size: "xl",
-                    quantity: Math.floor(Math.random() * 20),
-                    productId: createdProduct.id,
-                },
-                {
-                    size: "xxl",
-                    quantity: Math.floor(Math.random() * 20),
-                    productId: createdProduct.id,
-                },
-            ],
-        });
-    };
-
-    await Promise.all([
-        createProductOne(),
-        createProductTwo(),
-        createProductThree(),
-        createProductFour(),
-        createProductFive(),
-    ]);
+    }
 }
 
 main()
